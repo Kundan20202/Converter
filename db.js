04a8e20efcc4a0b5edcb2ee14b6e29855d072b35
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2024-11-29 05:24:21 +0530

    db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Client } = require('pg');
 
 // Connection string from Render
 const client = new Client({
-  connectionString: 'postgresql://web_to_app_db_user:qFqUzCUmLZ8KHYZmCdECNnUfjtV82pdP@dpg-ct4buaggph6c73c6fatg-a.oregon-postgres.render.com/web_to_app_db', // Replace with the Render connection string
+  connectionString: 'postgresql://web_to_app_db_user:qFqUzCUmLZ8KHYZmCdECNnUfjtV82pdP@dpg-ct4buaggph6c73c6fatg-a/web_to_app_db', // Replace with the Render connection string
   ssl: {
     rejectUnauthorized: false
   }
