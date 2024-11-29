cf851bfe7a4099470bb960d3173d8476a83fff80
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2024-11-29 20:17:14 +0530

    db.js

--- db.js
+++ db.js
@@ -1,20 +1,10 @@
-// db.js
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
-// Connection string from Render
-const client = new Client({
-  connectionString: 'postgresql://web_to_app_db_user:qFqUzCUmLZ8KHYZmCdECNnUfjtV82pdP@dpg-ct4buaggph6c73c6fatg-a/web_to_app_db', // Replace with the Render connection string
+const pool = new Pool({
+  connectionString: 'postgresql://web_to_app_db_user:qFqUzCUmLZ8KHYZmCdECNnUfjtV82pdP@dpg-ct4buaggph6c73c6fatg-a/web_to_app_db',
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false, // Required for Render
+  },
 });
 
-client.connect()
-  .then(() => {
-    console.log("PostgreSQL connected!");
-  })
-  .catch(err => {
-    console.error("Connection error", err.stack);
-  });
-
-module.exports = client;
+module.exports = pool;
