844fc45c20264d9fb499d6666fbbd0e5928cc460
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2024-11-29 21:15:59 +0530

    schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,7 @@
-
 CREATE TABLE submissions (
-  id SERIAL PRIMARY KEY,
-  app_name VARCHAR(255) NOT NULL,
-  website VARCHAR(255) NOT NULL,
-  app_type VARCHAR(50) NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW()
+    id SERIAL PRIMARY KEY,
+    app_name VARCHAR(255) NOT NULL,
+    website TEXT NOT NULL,
+    app_type VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
