9937e85ee464301bb60511df3cf6f63f1a751a7d
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2024-12-03 01:48:45 +0530

    schema.sql

--- schema.sql
+++ schema.sql
@@ -3,5 +3,6 @@ CREATE TABLE submissions (
     app_name VARCHAR(255) NOT NULL,
     website TEXT NOT NULL,
     app_type VARCHAR(50) NOT NULL,
+    app_link TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
