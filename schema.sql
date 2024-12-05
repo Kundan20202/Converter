9a5aac82d8794f86f0550b63baca1799d09108aa
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2024-12-05 23:58:44 +0530

    schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,10 @@
-CREATE TABLE IF NOT EXISTS apps (
+DROP TABLE IF EXISTS apps;
+
+CREATE TABLE apps (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    website VARCHAR(255), -- Ensure 'website' column is included
+    website VARCHAR(255),
     app_name VARCHAR(255) NOT NULL,
     app_url TEXT,
     created_at TIMESTAMP DEFAULT NOW()
