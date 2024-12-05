e53af8102d3a49b14ab151637971ebf22b704fd6
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2024-12-06 01:29:04 +0530

    schema.sql

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE apps (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    website VARCHAR(255),
+    website VARCHAR(255) NOT NULL,
     app_name VARCHAR(255) NOT NULL,
     app_url TEXT,
     created_at TIMESTAMP DEFAULT NOW()
