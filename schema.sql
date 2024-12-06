4eee3232092866ca6cb989a3cf44ae43f4343470
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2024-12-07 04:57:18 +0530

    schema.sql

--- schema.sql
+++ schema.sql
@@ -1,11 +1,15 @@
+
 DROP TABLE IF EXISTS apps;
 
 CREATE TABLE apps (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    website VARCHAR(255) NOT NULL,
-    app_name VARCHAR(255) NOT NULL,
-    app_url TEXT,
-    created_at TIMESTAMP DEFAULT NOW()
+    id SERIAL PRIMARY KEY,             
+    name VARCHAR(255) NOT NULL,        
+    email VARCHAR(255) NOT NULL,       
+    website VARCHAR(255) NOT NULL,     
+    app_name VARCHAR(255) NOT NULL,    
+    app_url TEXT,                      
+    created_at TIMESTAMP DEFAULT NOW() 
 );
+SELECT column_name
+FROM information_schema.columns
+WHERE table_name = 'apps';
