40518382d03c6820a51d73836ed02c1fa32c0fe1
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2024-12-07 03:58:30 +0530

    index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const createTableQuery = `
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL,
-        website VARCHAR(255), -- Ensure 'website' column is included
+        website VARCHAR(255),
         app_name VARCHAR(255) NOT NULL,
         app_url TEXT,
         created_at TIMESTAMP DEFAULT NOW()
