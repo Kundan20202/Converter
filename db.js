8b244df6859cedd1a9fd33ed4350cb5ba2154b7d
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2025-01-03 22:13:40 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,9 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: 'DATABASE_URL='postgresql://converter_owner:YW4ieXoLaO1U@ep-twilight-cell-a5105fe9.us-east-2.aws.neon.tech/converter?sslmode=require',
-  ssl: {
-    rejectUnauthorized: false, // Required for Render
-  },
+  
 });
 
 module.exports = pool;
