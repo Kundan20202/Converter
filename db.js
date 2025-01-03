1726f0aec65440d19d577f5e318beaa4946a9edf
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2025-01-03 21:52:58 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://web_to_app_db_user:qFqUzCUmLZ8KHYZmCdECNnUfjtV82pdP@dpg-ct4buaggph6c73c6fatg-a/web_to_app_db',
+  connectionString: 'DATABASE_URL='postgresql://converter_owner:YW4ieXoLaO1U@ep-twilight-cell-a5105fe9.us-east-2.aws.neon.tech/converter?sslmode=require',
   ssl: {
     rejectUnauthorized: false, // Required for Render
   },
