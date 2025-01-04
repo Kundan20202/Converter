730b33e616b8494a514ae56d287c7f34fc302ca0
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2025-01-05 02:42:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ const createTableQuery = `
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     website VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     app_name VARCHAR(255) NOT NULL,
     app_url TEXT,
     created_at TIMESTAMP DEFAULT NOW()
