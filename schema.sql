1e5a1096bd13d4478298bcbc98740f8e2036e146
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2025-01-05 03:04:34 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,16 +1,25 @@
-
 DROP TABLE IF EXISTS apps;
 
 CREATE TABLE apps (
-    id SERIAL PRIMARY KEY,             
-    name VARCHAR(255) NOT NULL,        
-    email VARCHAR(255) NOT NULL,       
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     website VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    app_name VARCHAR(255) NOT NULL,    
-    app_url TEXT,                      
-    created_at TIMESTAMP DEFAULT NOW() 
+    situation VARCHAR(255) NOT NULL,
+    app_name VARCHAR(255) NOT NULL,
+    app_type VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    icon TEXT,
+    splash_image TEXT,
+    visitors INTEGER,
+    features TEXT[],
+    app_design VARCHAR(255) NOT NULL,
+    customization TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
 );
+
 SELECT column_name
 FROM information_schema.columns
 WHERE table_name = 'apps';
