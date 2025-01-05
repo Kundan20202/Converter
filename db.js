7943fd1240638c954f1ceecf27b561c4e1a8865a
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2025-01-05 20:00:28 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'DATABASE_URL='',
+  connectionString: 'DATABASE_URL=postgresql://converter_owner:YW4ieXoLaO1U@ep-twilight-cell-a5105fe9.us-east-2.aws.neon.tech/converter?sslmode=require',
   ssl: {
     rejectUnauthorized: false  // Adjust if you're encountering SSL verification errors
   }
