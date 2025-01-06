44abe6df9a743dfb9f06f2b0fd279423a36ebe2d
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2025-01-07 02:06:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -100,6 +100,15 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const corsOptions = {
+    origin: 'https://stackblitzstartersfaqe1hfj-ler5--8080--c8c182a3.local-credentialless.webcontainer.io/', // Replace with your StackBlitz URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
+
+
 
 // Root Route
 app.get('/', (req, res) => {
