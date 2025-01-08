d8893fd35150015c885b0d217c0abf0cf277174b
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2025-01-08 07:45:57 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -9,13 +9,13 @@ CREATE TABLE apps (
     situation VARCHAR(50) DEFAULT 'Pending',
     app_name VARCHAR(255) DEFAULT 'DefaultAppName',
     app_type VARCHAR(50) DEFAULT NULL,
+    visitors TEXT DEFAULT 0,
     country VARCHAR(50) DEFAULT NULL,
     icon TEXT DEFAULT NULL,
     splash_image TEXT DEFAULT NULL,
     customization JSON DEFAULT NULL,
     features TEXT DEFAULT NULL,
     app_design TEXT DEFAULT NULL,
-    visitors INTEGER DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
